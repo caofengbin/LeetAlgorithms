5d21444c43205d0e156af0d6d8b02b29db4c254c
caofengbin <1050430934@qq.com>
2018-08-09 09:54:57 +0800

    init projects

---
+++ mazingwang/001_Two_Sum.py
@@ -0,0 +1,14 @@
+# links:https://leetcode.com/problems/two-sum/description/
+# Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# 给定一个无序的数组以及一个元素target，返回等于该元素target的两个元素的下标，注意同一个元素不能使用两次
+
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+
